bdd3fb5d9c2c428f252bceb3ad1170a45b93f833
Reiss Florian <85802@supinfo.com>
2013-05-17 11:20:00 -0700

    File Size
    Well... Method correct but doestn't work on the site.
    Problem with the filename...

---
+++ CodeEval_Easy/FileSize.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+namespace CodeEval_Easy
+{
+    class FileSize
+    {
+        public FileSize(string line)
+        {
+            FileInfo f = new FileInfo(line);
+            System.Console.WriteLine(f.Length);
+        }
+    }
+}
