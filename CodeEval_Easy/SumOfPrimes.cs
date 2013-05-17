d0d0f9d7c11bfa134281400cf737adb85cad457f
Reiss Florian <85802@supinfo.com>
2013-05-16 17:22:16 -0700

    Sum Of Primes.

---
+++ CodeEval_Easy/SumOfPrimes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeEval_Easy
+{
+    class SumOfPrimes
+    {
+        public SumOfPrimes()
+        {
+            int j = 0;
+            int i = 2;
+            int number = 0;
+            while(j != 1000)
+            {
+                if(isPrime(i))
+                {
+                    number += i;
+                    j++;
+                }
+                i++;
+            }
+
+            Console.WriteLine(number);
+        }
+
+        public static bool isPrime(int number)
+        {
+            for(int i = 2 ; i <= Math.Sqrt(number) ; i++)
+            {
+                if(number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
