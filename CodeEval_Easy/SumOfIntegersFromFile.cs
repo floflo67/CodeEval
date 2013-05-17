36659fbcd1ac832a17550d8344d83f1d9fc8c61a
Reiss Florian <85802@supinfo.com>
2013-05-16 19:41:09 -0700

    Sum Of Integers From File.

---
+++ CodeEval_Easy/SumOfIntegersFromFile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace CodeEval_Easy
+{
+    class SumOfIntegersFromFile
+    {
+        public SumOfIntegersFromFile(List<int> list)
+        {
+            int result = 0;
+            foreach(int i in list)
+                result += i;
+
+            System.Console.WriteLine(result);
+        }
+    }
+}
