73a197beee9fabd4edad7d0e0c3b308eb38240fe
Reiss Florian <85802@supinfo.com>
2013-05-16 20:08:21 -0700

    Set Intersection

---
+++ CodeEval_Easy/SetIntersection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeEval_Easy
+{
+    class SetIntersection
+    {
+        public SetIntersection(string line)
+        {
+            string[] str = line.Split(new char[] { ';' });
+            string[] str1 = str[0].Split(new char[] { ',' });
+            string[] str2 = str[1].Split(new char[] { ',' });
+
+            List<int> list1 = new List<int>();
+            List<int> list2 = new List<int>();
+
+            foreach(string s in str1)
+            {
+                list1.Add(int.Parse(s));
+            }
+
+            foreach(string s in str2)
+            {
+                list2.Add(int.Parse(s));
+            }
+
+            List<int> result = list1.Distinct().Intersect(list2.Distinct()).Distinct().ToList();
+
+            string output = "";
+            foreach(int i in result)
+            {
+                if(i == result.Last())
+                    output += i.ToString();
+                else
+                    output += i.ToString() + ",";
+            }
+            System.Console.WriteLine(output);
+        }
+    }
+}
