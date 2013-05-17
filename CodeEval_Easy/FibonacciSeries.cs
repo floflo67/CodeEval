d9ce78d583b648bd465b0c04569c415955f0b8d5
Reiss Florian <85802@supinfo.com>
2013-05-16 19:13:42 -0700

    Fibonacci Series

---
+++ CodeEval_Easy/FibonacciSeries.cs
@@ -0,0 +1,21 @@
+﻿namespace CodeEval_Easy
+{
+    class FibonacciSeries
+    {
+        public FibonacciSeries(string line)
+        {
+            int number = int.Parse(line);
+            System.Console.WriteLine(fib(number));
+        }
+
+        public int fib(int value)
+        {
+            if(value == 0)
+                return 0;
+            if(value == 1)
+                return 1;
+            else
+                return fib(value - 1) + fib(value - 2);
+        }
+    }
+}
