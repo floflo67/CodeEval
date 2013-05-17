62fef02337edf102cd70ebaf7f47a78531eb224a
Reiss Florian <85802@supinfo.com>
2013-05-16 19:09:18 -0700

    Sum Of Digits.

---
+++ CodeEval_Easy/SumOfDigits.cs
@@ -0,0 +1,15 @@
+﻿namespace CodeEval_Easy
+{
+    class SumOfDigits
+    {
+        public SumOfDigits(string line)
+        {
+            int result = 0;
+            foreach(char c in line)
+            {
+                result += int.Parse(c.ToString());
+            }
+            System.Console.WriteLine(result);
+        }
+    }
+}
