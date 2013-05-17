80d9c7402870820d31583c9131cf3a85d1a14da8
Reiss Florian <85802@supinfo.com>
2013-05-16 20:56:22 -0700

    Rightmost Char.

---
+++ CodeEval_Easy/RightmostChar.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace CodeEval_Easy
+{
+    class RightmostChar
+    {
+        public RightmostChar(string line)
+        {
+            string[] str = line.Split(new char[] { ',' });
+            int index = str[0].IndexOf(str[1]);
+            System.Console.WriteLine(index);
+        }
+    }
+}
