72aa15cc12f3ee484f8be14493e4821534d2abda
Reiss Florian <85802@supinfo.com>
2013-05-17 14:04:55 -0700

    M To Last Element.

---
+++ CodeEval_Moderate/MToLastElement.cs
@@ -0,0 +1,15 @@
+﻿namespace CodeEval_Moderate
+{
+    class MToLastElement
+    {
+        public MToLastElement(string line)
+        {
+            string[] str = line.Split(new char[] { ' ' });
+            int length = str.Length;
+            int M = int.Parse(str[length - 1]);
+
+            if(M < length)
+                System.Console.WriteLine(str[length - M - 1]);
+        }
+    }
+}
