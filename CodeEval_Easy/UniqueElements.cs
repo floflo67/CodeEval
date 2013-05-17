ee39b7b8551e7a44744550cc83787c183e246608
Reiss Florian <85802@supinfo.com>
2013-05-16 20:01:38 -0700

    Unique elements.

---
+++ CodeEval_Easy/UniqueElements.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeEval_Easy
+{
+    class UniqueElements
+    {
+        public UniqueElements(string line)
+        {
+            string[] str = line.Split(new char[] { ',' });
+            List<int> list = new List<int>();
+            foreach(string s in str)
+            {
+                list.Add(int.Parse(s));
+            }
+            list = list.Distinct().ToList();
+            string output = "";
+            foreach(int i in list)
+            {
+                if(i == list.Last())
+                    output += i.ToString();
+                else
+                    output+= i.ToString() + ",";
+            }
+            System.Console.WriteLine(output);
+        }
+    }
+}
