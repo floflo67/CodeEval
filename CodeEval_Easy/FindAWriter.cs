33661e92fec6c3079964fb3ef053dc5e516d1f07
Reiss Florian <85802@supinfo.com>
2013-05-17 10:48:25 -0700

    Find A Writer.

---
+++ CodeEval_Easy/FindAWriter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeEval_Easy
+{
+    class FindAWriter
+    {
+        public FindAWriter(string line)
+        {
+            string[] str = line.Split(new char[] { '|' });
+            string[] indexes = str[1].Split(new char[] { ' ' });
+            string output = "";
+
+            List<int> index = new List<int>();
+
+            foreach(string s in indexes)
+            {
+                int ind = 0;
+                int.TryParse(s, out ind);
+                if(ind != 0)
+                    index.Add(ind);
+            }
+
+            foreach(int i in index)
+                output += str[0].ElementAt(i - 1);
+
+            System.Console.WriteLine(output);
+        }
+    }
+}
