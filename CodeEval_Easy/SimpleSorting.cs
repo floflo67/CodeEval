b89753b6c695986f4d6a260d424d724f8faf2576
Reiss Florian <85802@supinfo.com>
2013-05-17 10:18:30 -0700

    Simple sorting.

---
+++ CodeEval_Easy/SimpleSorting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeEval_Easy
+{
+    class SimpleSorting
+    {
+        public SimpleSorting(string line)
+        {
+            string[] str = line.Split(new char[] { ' ' });
+            List<double> list = new List<double>();
+            string output = "";
+
+            foreach(string s in str)
+            {
+                list.Add(double.Parse(s));
+            }
+
+            list.Sort();
+
+            foreach(double i in list)
+            {
+                if(i == list.Last())
+                    output += i.ToString("0.000");
+                else
+                    output += i.ToString("0.000") + " ";
+            }
+
+            System.Console.WriteLine(output);
+        }
+    }
+}
