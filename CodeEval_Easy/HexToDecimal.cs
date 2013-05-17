4d7419b5ca685de8524ba05feb2bd98ce7e0ab0c
Reiss Florian <85802@supinfo.com>
2013-05-16 21:57:36 -0700

    Hex To Decimal

---
+++ CodeEval_Easy/HexToDecimal.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+
+namespace CodeEval_Easy
+{
+    class HexToDecimal
+    {
+        public HexToDecimal(string line)
+        {
+            int length = line.Length;
+            double result = 0;
+            for(int i = 0 ; i < length ; i++)
+            {
+                result += hexConvert(line.Substring(length - i - 1, 1), i);
+            }
+            System.Console.WriteLine(result);
+        }
+
+        public double hexConvert(string number, int rank)
+        {
+            int value = 0;
+            double val = 0;
+            if(number.ToLower() == "a")
+                value = 10;
+            else if(number.ToLower() == "b")
+                value = 11;
+            else if(number.ToLower() == "c")
+                value = 12;
+            else if(number.ToLower() == "d")
+                value = 13;
+            else if(number.ToLower() == "e")
+                value = 14;
+            else if(number.ToLower() == "f")
+                value = 15;
+            else
+                value = int.Parse(number);
+            val = value * System.Math.Pow(16, rank);
+            return val;
+        }
+    }
+}
