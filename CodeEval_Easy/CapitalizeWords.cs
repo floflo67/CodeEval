84495259a7ae2d3346209d2f37ffd56d2f8e186f
Reiss Florian <85802@supinfo.com>
2013-05-17 10:27:16 -0700

    Capitalize Words

---
+++ CodeEval_Easy/CapitalizeWords.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeEval_Easy
+{
+    class CapitalizeWords
+    {
+        public CapitalizeWords(string line)
+        {
+            string[] str = line.Split(new char[] { ' ' });
+            string output = "";
+            foreach(string s in str)
+            {
+                if(s == str.Last())
+                    output += s.Substring(0, 1).ToUpper() + s.Substring(1);
+                else
+                    output += s.Substring(0, 1).ToUpper() + s.Substring(1) + " ";
+            }
+            System.Console.WriteLine(output);
+        }
+    }
+}
