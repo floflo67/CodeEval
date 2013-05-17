f10f02db39546fc94f8ddc5cc4cad1139e195914
Reiss Florian <85802@supinfo.com>
2013-05-17 10:22:01 -0700

    Penultimate Word.

---
+++ CodeEval_Easy/PenultimateWord.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeEval_Easy
+{
+    class PenultimateWord
+    {
+        public PenultimateWord(string line)
+        {
+            string[] str = line.Split(new char[] { ' ' });
+            int max = str.Length;
+
+            System.Console.WriteLine(str[max-2]);
+        }
+    }
+}
