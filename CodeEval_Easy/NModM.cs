75ff23d4ca0c21b518d5e19bac6c68810a48aeb3
Reiss Florian <85802@supinfo.com>
2013-05-16 21:32:43 -0700

    N Mod M

---
+++ CodeEval_Easy/NModM.cs
@@ -0,0 +1,19 @@
+﻿namespace CodeEval_Easy
+{
+    class NModM
+    {
+        public NModM(string line)
+        {
+            string[] str = line.Split(new char[] { ',' });
+            new NModM(int.Parse(str[0]), int.Parse(str[1]));
+        }
+
+        public NModM(int N, int M)
+        {
+            while(N >= M)
+                N -= M;
+
+            System.Console.WriteLine(N);
+        }
+    }
+}
