d5dca35f05d51dab4e6fba8e9200e7503f72f35c
Reiss Florian <85802@supinfo.com>
2013-05-17 11:36:05 -0700

    Longest Lines

---
+++ CodeEval_Moderate/LongestLines.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeEval_Moderate
+{
+    class LongestLines
+    {
+        public LongestLines(int N, List<string> list)
+        {
+            list = list.OrderByDescending(x => x.Length).ToList();
+            for(int i = 0 ; i < N ; i++)
+                System.Console.WriteLine(list[i]);
+        }
+    }
+}
