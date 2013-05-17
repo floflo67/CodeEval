27e8a68c2f3dd729ae99d2f23dac5d79a8925c2f
Reiss Florian <85802@supinfo.com>
2013-05-16 18:13:25 -0700

    Reverse Words.

---
+++ CodeEval_Easy/ReverseWords.cs
@@ -0,0 +1,18 @@
+﻿namespace CodeEval_Easy
+{
+    class ReverseWords
+    {
+        public ReverseWords(string line)
+        {
+            string[] str = line.Split(new char[] { ' ' });
+            string output = "";
+
+            foreach(string s in str)
+            {
+                output = s + " " + output;
+            }
+
+            System.Console.WriteLine(output);
+        }
+    }
+}
