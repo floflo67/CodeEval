222faa11a90ce9db92307e1ceba9676312f571e2
Reiss Florian <85802@supinfo.com>
2013-05-17 10:38:16 -0700

    Swap Case.

---
+++ CodeEval_Easy/SwapCase.cs
@@ -0,0 +1,19 @@
+﻿namespace CodeEval_Easy
+{
+    class SwapCase
+    {
+        public SwapCase(string line)
+        {
+            string output = "";
+            foreach(char c in line)
+            {
+                if(System.Char.IsUpper(c))
+                    output += System.Char.ToLower(c);
+                else
+                    output += System.Char.ToUpper(c);
+            }
+
+            System.Console.WriteLine(output);
+        }
+    }
+}
