9a9d1e1712f60e9b6f82a5f9a3cd0aa3136038dc
Reiss Florian <85802@supinfo.com>
2013-05-17 14:42:11 -0700

    Prime Numbers.

---
+++ CodeEval_Moderate/PrimeNumbers.cs
@@ -0,0 +1,33 @@
+﻿namespace CodeEval_Moderate
+{
+    class PrimeNumbers
+    {
+        public PrimeNumbers(string line)
+        {
+            int N = int.Parse(line);
+            string output = "2";
+            if(N < 2)
+                System.Console.WriteLine("NULL");
+            else
+            {
+                for(int i = 3 ; i < N ; i = i + 2)
+                {
+                    if(isPrime(i))
+                        output += "," + i;
+                }
+                System.Console.WriteLine(output);
+            }
+        }
+
+        public bool isPrime(int number)
+        {
+            for(int i = 2 ; i <= System.Math.Sqrt(number) ; i++)
+            {
+                if(number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
