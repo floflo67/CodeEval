a035beab079d86e584d79dfa371694c7f407c2e5
Reiss Florian <85802@supinfo.com>
2013-05-16 19:44:41 -0700

    Odd Numbers

---
+++ CodeEval_Easy/OddNumbers.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeEval_Easy
+{
+    class OddNumbers
+    {
+        public OddNumbers()
+        {
+            for(int i = 1 ; i <= 99 ; i += 2)
+            {
+                System.Console.WriteLine(i);
+            }
+        }
+    }
+}
