d496b26e564ff6db08e068e46e901d5a3bc4ed94
Reiss Florian <85802@supinfo.com>
2013-05-16 18:22:03 -0700

    Multiples of a number

---
+++ CodeEval_Easy/MultiplesOfANumber.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeEval_Easy
+{
+    class MultiplesOfANumber
+    {
+        public MultiplesOfANumber(string line)
+        {
+            string[] str = line.Split(new char[] { ',' });
+            new MultiplesOfANumber(int.Parse(str[0]), int.Parse(str[1]));
+        }
+
+        public MultiplesOfANumber(int x, int n)
+        {
+            for(int i = 1 ; i < x ; i++)
+            {
+                if(i * n >= x)
+                {
+                    System.Console.WriteLine(i * n);
+                    break;
+                }
+            }
+        }
+    }
+}
