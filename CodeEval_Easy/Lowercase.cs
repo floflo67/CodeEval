d7de22e7fb2196ab9bb2c5cc726b7248a2e84b12
Reiss Florian <85802@supinfo.com>
2013-05-16 19:06:33 -0700

    Lowercase

---
+++ CodeEval_Easy/Lowercase.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeEval_Easy
+{
+    class Lowercase
+    {
+        public Lowercase(string line)
+        {
+            System.Console.WriteLine(line.ToLower());
+        }
+    }
+}
