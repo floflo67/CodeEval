67d1ec3e5005785300000367df4b9a94634e64ec
Reiss Florian <85802@supinfo.com>
2013-05-16 19:30:17 -0700

    Multiplication Tables

---
+++ CodeEval_Easy/MultiplicationTables.cs
@@ -0,0 +1,22 @@
+﻿namespace CodeEval_Easy
+{
+    class MultiplicationTables
+    {
+        public MultiplicationTables()
+        {
+            for(int i = 1 ; i <= 12 ; i++)
+            {
+                string output = "";
+                for(int j = 1 ; j <= 12 ; j++)
+                {
+                    string k = (i * j).ToString();
+                    while(k.Length < 4)
+                        k = " " + k;
+
+                    output += k;
+                }
+                System.Console.WriteLine(output);
+            }
+        }
+    }
+}
