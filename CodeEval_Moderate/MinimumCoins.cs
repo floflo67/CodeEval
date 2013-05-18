c0662887f6941fcffe5c793d564cc4a2eabd2651
Reiss Florian <85802@supinfo.com>
2013-05-17 20:22:16 -0700

    Minimum Coins.

---
+++ CodeEval_Moderate/MinimumCoins.cs
@@ -0,0 +1,18 @@
+﻿namespace CodeEval_Moderate
+{
+    class MinimumCoins
+    {
+        public MinimumCoins(string line)
+        {
+            double number = double.Parse(line);
+            double numFive = System.Math.Truncate(number / 5.00);
+            number -= 5 * numFive;
+            double numThree = System.Math.Truncate(number / 3.00);
+            number -= 3 * numThree;
+            double numOne = System.Math.Truncate(number / 1.00);
+            number -= 1 * numOne;
+
+            System.Console.WriteLine(numOne + numThree + numFive);
+        }
+    }
+}
