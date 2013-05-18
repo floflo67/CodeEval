f4892cee5ebf8353888ae3e5df9cd4b10d01f64c
Reiss Florian <85802@supinfo.com>
2013-05-17 19:23:53 -0700

    Array Absurdity

---
+++ CodeEval_Moderate/ArrayAbsurdity.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeEval_Moderate
+{
+    class ArrayAbsurdity
+    {
+        public ArrayAbsurdity(string line)
+        {
+            string[] str = line.Split(new char[] { ';' });
+            string[] array = str[1].Split(new char[] { ',' });
+            List<string> list = new List<string>();
+
+            foreach(string s in array)
+            {
+                if(list.Contains(s))
+                    System.Console.WriteLine(s);
+                else
+                    list.Add(s);
+            }
+        }
+    }
+}
