8ed24d3ba197bc54319c225d090dfa912f58d4a5
Reiss Florian <85802@supinfo.com>
2013-05-17 18:58:01 -0700

    Remove Characters.

---
+++ CodeEval_Moderate/RemoveCharacters.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeEval_Moderate
+{
+    class RemoveCharacters
+    {
+        public RemoveCharacters(string line)
+        {
+            string[] str = line.Split(new char[] { ',' });
+            string output = str[0];
+            string temp = "";
+
+            foreach(char c in str[1].Trim())
+            {
+                temp = output;
+                output = "";
+                for(int i = 0 ; i < temp.Length ; i++)
+                {
+                    if(temp[i] != c)
+                        output += temp[i];
+                }
+            }
+
+            System.Console.WriteLine(output);
+        }
+    }
+}
