945af4aaef4384d5912e96a4596b8de61aaf4769
Reiss Florian <85802@supinfo.com>
2013-05-17 20:09:03 -0700

    Counting Primes.

---
+++ CodeEval_Moderate/CountingPrimes.cs
@@ -0,0 +1,38 @@
+﻿namespace CodeEval_Moderate
+{
+    class CountingPrimes
+    {
+        public CountingPrimes(string line)
+        {
+            string[] str = line.Split(new char[] { ',' });
+            int N = int.Parse(str[0]);
+            int M = int.Parse(str[1]);
+
+            int s = CountPrimes(N, M);
+            System.Console.WriteLine(s);
+        }
+
+        public int CountPrimes(int N, int M)
+        {
+            int j = 0;
+
+            for(int i = N ; i <= M ; i++)
+            {
+                if(isPrime(i))
+                    j++;
+            }
+            return j;
+        }
+
+        public bool isPrime(int number)
+        {
+            for(int i = 2 ; i <= System.Math.Sqrt(number) ; i++)
+            {
+                if(number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
