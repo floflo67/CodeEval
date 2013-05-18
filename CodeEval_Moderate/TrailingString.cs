cca9a9c2fced0885a96b1e04b04b17d66851f0a8
Reiss Florian <85802@supinfo.com>
2013-05-17 19:03:14 -0700

    Trailing String.

---
+++ CodeEval_Moderate/TrailingString.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace CodeEval_Moderate
+{
+    class TrailingString
+    {
+        public TrailingString(string line)
+        {
+            string[] str = line.Split(new char[] { ',' });
+            if(str[0].EndsWith(str[1]))
+                System.Console.WriteLine("1");
+            else
+                System.Console.WriteLine("0");
+        }
+    }
+}
